09a87461e73cd95dc95450446014b8fa8fc31c14
Jesus-Eder-Gonzalez <jesus.eder.gonzalez@gmail.com>
2018-08-13 19:26:18 -1000

    Added express to the project.

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 8080;
+const app = express();
+const routes = require('./routes');
+
+app.use(bodyParser.json());
+
+app.use('/', routes);
+
+app.listen(PORT, 'localhost', () => {
+  console.log(`SERVER LISTENING ON PORT ${PORT}`);
+});
